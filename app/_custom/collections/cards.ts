33c33fb9ae21fbb0920b8262d31eb6bf09fc3b25
Nick Winters <nick@pogseal.com>
2024-10-14 11:49:38 -0700

    Update cards.ts

--- app/_custom/collections/cards.ts
+++ app/_custom/collections/cards.ts
@@ -33,6 +33,17 @@ export const Cards: CollectionConfig = {
          type: "text",
       },
       {
+         name: "rating",
+         type: "select",
+         options: [
+            { label: "1 Star", value: "1" },
+            { label: "2 Stars", value: "2" },
+            { label: "3 Stars", value: "3" },
+            { label: "4 Stars", value: "4" },
+            { label: "5 Stars", value: "5" },
+         ],
+      },
+      {
          name: "icon",
          type: "upload",
          relationTo: "images",
