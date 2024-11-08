b35b00cfba57ff0837fc3d6968e4b578c7a8ddfa
Nick Winters <nick@pogseal.com>
2024-11-08 10:24:47 -0800

    Update decks.ts

--- app/_custom/collections/decks.ts
+++ app/_custom/collections/decks.ts
@@ -1,8 +1,4 @@
 import type { CollectionConfig } from "payload/types";
-import {
-   afterDeleteSearchSyncHook,
-   afterChangeSearchSyncHook,
-} from "../hooks/search-hooks";
 
 import { isStaff } from "../../db/collections/users/users.access";
 import { User } from "payload/dist/auth";
@@ -19,10 +15,6 @@ export const Decks: CollectionConfig = {
       read: () => true,
       update: isStaff,
       delete: isStaff,
-   },
-   hooks: {
-      afterDelete: [afterDeleteSearchSyncHook],
-      afterChange: [afterChangeSearchSyncHook],
    },
    fields: [
       {
